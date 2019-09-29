8aab948e2bcb11592864d2a733b854d01e1a1a07
秉虎 <s96016641@gmail.com>
2019-09-30 03:59:44 +0800

    refactor: using factory methods
    rewrite app.py by using factory methods

--- app.py
+++ app.py
@@ -5,45 +5,46 @@ from blueprints.api import api as bp_api
 from blueprints.login import login as bp_login, login_manager
 from database import db
 
-app = Flask(__name__)
-app.secret_key = b'(ML\x90\x13\xcd\xaev\xa0 \x1d\x1fC\xab\xb7\x05'
 
-# disable sorting the jsonify data
-app.config['JSON_SORT_KEYS'] = False
+def create_app(config_file=None) -> Flask:
+    if config_file is None:
+        app = Flask(__name__)
+        # testing mode, should be production if testing done
+        app.secret_key = b'(ML\x90\x13\xcd\xaev\xa0 \x1d\x1fC\xab\xb7\x05'
 
-# database connect pre define, must as first as possible
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:meowmeow@127.0.0.1:32769/testdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db.init_app(app)
-login_manager.init_app(app)
+        # disable sorting the jsonify data
+        app.config['JSON_SORT_KEYS'] = False
 
-# import blueprints to flask
-app.register_blueprint(bp_api)
-app.register_blueprint(bp_login)
+        # database connect pre define, must as first as possible
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:meowmeow@127.0.0.1:32769/testdb'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+        db.init_app(app)
+        login_manager.init_app(app)
 
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+        # import blueprints to flask
+        app.register_blueprint(bp_api)
+        app.register_blueprint(bp_login)
 
+        CORS(app, resources={r"/api/*": {"origins": "*"}})
+    else:
+        app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return redirect("/api")
+    @app.route('/')
+    def root():
+        return redirect("/api")
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response(jsonify({
+            "error": "Not found",
+            "reason": error.get_description()[3:-4]
+        }), 404)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({
-        "error": "Not found",
-        "reason": error.get_description()[3:-4]
-    }), 404)
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return make_response(jsonify({
+            "error": "Internal Server Error",
+            "reason": error.get_description()[3:-4]
+        }), 500)
 
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return make_response(jsonify({
-        "error": "Internal Server Error",
-        "reason": error.get_description()[3:-4]
-    }), 500)
-
-
-if __name__ == '__main__':
-    app.run()
+    return app
