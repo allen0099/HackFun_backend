64180efe6a144b55b8c2b291f482f2e5ddcfb2bb
秉虎 <s96016641@gmail.com>
2020-05-23 17:50:27 +0800

    feat: Add method for check is this solved
    new method for check is the practice is solved

--- app/models/complete.py
+++ app/models/complete.py
@@ -37,3 +37,13 @@ class Complete(db.Model):
     def add(user_id: str, practice_uuid: str) -> None:
         db.session.add(Complete(user_id, practice_uuid))
         db.session.commit()
+
+    @staticmethod
+    def is_solved(user_id: str, practice_uuid: str) -> bool:
+        complete: Complete = Complete.query \
+            .filter_by(user_id=user_id) \
+            .filter_by(practice_uuid=practice_uuid) \
+            .first()
+        if complete is not None:
+            return True
+        return False
